ef45e0a40aaaee35f376e0708f84168af336ab48
omochanocha <yokpef@gmail.com>
2023-12-15 18:27:44 +0900

    tsconfig/strictestの導入

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const title = import.meta.env.VITE_APP_TITLE;
+const title = import.meta.env['VITE_APP_TITLE'];
 console.dir(import.meta.env);
 
 const App: FC = () => {
